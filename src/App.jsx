377920df04c0ff5b7cc12be152f61f0ff19201fb
Muhammed Al-lami <muhammedhn7@gmail.com>
2024-11-07 23:12:03 +0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,53 @@
-
 import "./App.css";
 import Sidebar from "../src/Componet/sidebar.jsx";
 import Right from "../src/Componet/right.jsx";
-import Number from "./Pages/number.jsx"
-import Vaild from "./Pages/pagevaild/vaild.jsx"
-import Email from "./Pages/email/email.jsx"
+import Number from "./Pages/number.jsx";
+import Vaild from "./Pages/pagevaild/vaild.jsx"; // Check spelling here
+import Email from "./Pages/email/email.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div className="main-box">
-                <Sidebar/>
-                <Right/>
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/number"
-            element={
-              <div className="main-box">
-                <Sidebar/>
-                <Number/>
-                
-              </div>
-            }
-          ></Route>
-          <Route
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="main-box">
+              <Sidebar />
+              <Right />
+            </div>
+          }
+        />
+        <Route
+          path="/number"
+          element={
+            <div className="main-box">
+              <Sidebar />
+              <Number />
+            </div>
+          }
+        />
+        <Route
           path="/vaild"
           element={
-            
             <div className="main-box">
-            <Sidebar/>
-            <Vaild/>
-          </div>
-          
-
+              <Sidebar />
+              <Vaild />
+            </div>
           }
-          />
-            <Route
+        />
+        <Route
           path="/email"
           element={
-            
             <div className="main-box">
-            <Sidebar/>
-            <Email/>
-          </div>
-          
-          
+              <Sidebar />
+              <Email />
+            </div>
           }
-          />
-        </Routes>
-      </Router>
-
-
-
-    </>
+        />
+      </Routes>
+    </Router>
   );
 }
 
